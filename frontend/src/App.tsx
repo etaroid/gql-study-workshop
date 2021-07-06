2782b18e65e2fa3696c04324f8fcd270c62a1efe
etaroid <amthgfonger.nk@gmail.com>
2021-07-06 18:57:15 +0900

    impl components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,35 @@
 import React from "react";
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 
+import { InMemoryCache, ApolloProvider, ApolloClient } from "@apollo/client";
+import Products from "./components/Products";
+import ProductDetail from "./components/ProductDetail";
+
 function App() {
+  // clientオブジェクトの作成
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "http://localhost:4010/graphql"
+  });
+
+  // 前提をApolloProviderでラップする
   return (
-    <HashRouter>
-      <Switch>
-        <Route path="/products" exact>
-          <div>TODO should be replaced to products list page</div>
-        </Route>
-        <Route path="/products/:productId">
-          <div>TODO should be replaced to product detail page</div>
-        </Route>
-        <Route>
-          <Redirect to="/products" />
-        </Route>
-      </Switch>
-    </HashRouter>
-  );
+      <ApolloProvider client={client}>
+        <HashRouter>
+          <Switch>
+            <Route path="/products" exact>
+                <Products />
+            </Route>
+            <Route path="/products/:productId">
+                <ProductDetail />
+            </Route>
+            <Route>
+              <Redirect to="/products" />
+            </Route>
+          </Switch>
+        </HashRouter>
+      </ApolloProvider>
+  )
 }
 
 export default App;
---
+++ frontend/src/components/ProductDetail.tsx
@@ -0,0 +1,65 @@
+import {gql, useMutation, useQuery} from "@apollo/client";
+import { useParams } from "react-router-dom";
+import {ProductDetailQuery, ProductDetailQueryVariables} from "./__generated__/product-detail-query";
+import {useState} from "react";
+import {AddReviewMutation, AddReviewMutationVariables} from "./__generated__/add-review-mutation";
+import ProductReview, {productReviewFragment} from "./ProductReview";
+
+const query = gql`
+    ${productReviewFragment}
+    query ProductDetailQuery($id: ID!) {
+        product(id: $id) {
+            id
+            name
+            description
+            ...ProductReviewFragment
+        }
+    }
+`;
+
+const mutation = gql`
+    mutation AddReviewMutation($pid: ID!, $comment: String!) {
+        addReview(
+            productId: $pid
+            addReviewInput: { commentBody: $comment, star: 0 }
+        ) {
+            id
+        }
+    }
+`;
+
+export default function () {
+    const { productId } = useParams<{ readonly productId: string }>();
+    const { data, loading, refetch } = useQuery<ProductDetailQuery, ProductDetailQueryVariables>(query, {
+        variables: {
+            id: productId
+        }
+    });
+
+    const [addReview, { loading: submitting }] = useMutation<
+        AddReviewMutation,
+        AddReviewMutationVariables
+    >(mutation, {
+        update(_, { data }) {
+            if (!data?.addReview) return;
+            refetch();
+        }
+    });
+
+    if (loading) return <div>loading...</div>;
+    if (!data?.product) return <div>not found</div>;
+    const { product } = data;
+
+    return (
+        <>
+            <h1>{product.name}</h1>
+            <p style={{ whiteSpace: "pre-wrap" }}>{product.description}</p>
+            <div>
+                <h2>レビュー</h2>
+                <ProductReview product={product} submitting={submitting} onSubmit={
+                    comment => addReview(({ variables: { pid: productId, comment } }))
+                } />
+            </div>
+        </>
+    );
+}
